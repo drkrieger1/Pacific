69d3adb7db8e0057ab80a8cd0baca999e2b3c01b
drkrieger1 <eplyushko@gmail.com>
2017-12-20 11:51:55 -0800

    working base site

--- Pacific/Pacific/Controllers/HomeController.cs
+++ Pacific/Pacific/Controllers/HomeController.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Pacific.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller 
     {
+        //IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public string Index()
+        //{
+        //    return "WtF?";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
