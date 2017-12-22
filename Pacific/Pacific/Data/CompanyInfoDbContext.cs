1f831fee8b3bfeed7dc5051cbeea3a89c76f2491
drkrieger1 <eplyushko@gmail.com>
2017-12-22 13:23:23 -0800

    db contexts filled out

---
+++ Pacific/Pacific/Data/CompanyInfoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pacific.Data
+{   
+    //Db Context for company information
+    public class CompanyInfoDbContext : DbContext 
+    {
+        public CompanyInfoDbContext(DbContextOptions<CompanyInfoDbContext>Options): base(Options)
+        {
+
+        }
+    }
+}
