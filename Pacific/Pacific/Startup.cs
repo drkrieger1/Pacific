2716c43bed3662e5c890e2f6a26ece1c5b575e31
drkrieger1 <eplyushko@gmail.com>
2017-12-22 14:01:50 -0800

    having connection string issues/ string is null

--- Pacific/Pacific/Startup.cs
+++ Pacific/Pacific/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Pacific.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Pacific
 {
@@ -24,6 +26,12 @@ namespace Pacific
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            //This is the Context for company info and leads database models
+            services.AddDbContext<CompanyInfoDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PacificContext")));
+            services.AddDbContext<LeadDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PacificContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
