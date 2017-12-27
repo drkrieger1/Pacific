7d0dbf0e2b694169ce6db5d11907790fae494f33
drkrieger1 <eplyushko@gmail.com>
2017-12-27 13:57:46 -0800

    fixed context issue, and finished init lead index method

--- Pacific/Pacific/Controllers/LeadController.cs
+++ Pacific/Pacific/Controllers/LeadController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pacific.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +9,26 @@ using System.Threading.Tasks;
 namespace Pacific.Controllers
 {
     public class LeadController : Controller 
-    {
+    {   
+        //Lead Db context dependency injection
+        private readonly LeadDbContext _Context; 
+        
+        //Cunstructor to requier a Db Context 
+        public LeadController(LeadDbContext context)
+        {
+            _Context = context;
+        }
+
+        //Index gathering All Leads from Db Context 
+        public async Task<IActionResult> Index(string searchString)
+        {
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                return View(await _Context.Lead.Where(c => c.Name.Contains(searchString)).ToListAsync());
+            }
+
+            return View(await _Context.Lead.ToListAsync());
+        }
+
     }
 }
