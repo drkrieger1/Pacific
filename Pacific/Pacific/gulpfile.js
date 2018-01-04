76db7154f7aa96a592f66d3a45374306f6ef7c6f
drkrieger1 <eplyushko@gmail.com>
2018-01-04 11:57:59 -0800

    gulp-sass compilation enabled

---
+++ Pacific/Pacific/gulpfile.js
@@ -0,0 +1,16 @@
+﻿/*
+This file is the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('compile-sass', function () {
+
+    // place code for your default task here
+    gulp.src('./wwwroot/lib/bootstrap/scss/bootstrap.scss')
+        .pipe(gulp())
+
+        .pipe(gulp.dest('./wwwroot/css')); 
+
+});
