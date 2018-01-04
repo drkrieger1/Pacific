9d054d19da1bec81bc15ed18af983b168194c5ac
drkrieger1 <eplyushko@gmail.com>
2018-01-04 12:29:27 -0800

    fixed a gulp issue, and installed font-awesome

--- Pacific/Pacific/gulpfile.js
+++ Pacific/Pacific/gulpfile.js
@@ -1,16 +1,33 @@
-﻿/*
+﻿/// <binding BeforeBuild='compile-sass' />
+/*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
 
+var gulpSass = require('gulp-sass');
+
 gulp.task('compile-sass', function () {
 
     // place code for your default task here
     gulp.src('./wwwroot/lib/bootstrap/scss/bootstrap.scss')
-        .pipe(gulp())
+        .pipe(gulpSass())
 
         .pipe(gulp.dest('./wwwroot/css')); 
 
 });
+
+//var gulp = require('gulp');
+
+//var gulpSass = require('gulp-sass');
+
+//gulp.task('compile-sass', function () {
+
+//    gulp.src('./wwwroot/lib/bootstrap/scss/bootstrap.scss')
+
+//        .pipe(gulpSass())
+
+//        .pipe(gulp.dest('./wwwroot/css'));
+
+//});
