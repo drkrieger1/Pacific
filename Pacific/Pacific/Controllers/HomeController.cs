8c3775c0dd3d6f709b0ab0c0c28619d98141ba4f
drkrieger1 <eplyushko@gmail.com>
2018-01-10 18:33:38 -0800

    submit button added to the contract form and wired up the nav links

--- Pacific/Pacific/Controllers/HomeController.cs
+++ Pacific/Pacific/Controllers/HomeController.cs
@@ -18,5 +18,15 @@ namespace Pacific.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
