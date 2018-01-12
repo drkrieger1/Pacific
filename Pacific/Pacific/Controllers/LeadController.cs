cd7a93a5dd86d33026ce393935856bcd7230aafa
drkrieger1 <eplyushko@gmail.com>
2018-01-12 12:48:43 -0800

    added date property to leads, create method sets the users local date time to their request

--- Pacific/Pacific/Controllers/LeadController.cs
+++ Pacific/Pacific/Controllers/LeadController.cs
@@ -38,13 +38,14 @@ namespace Pacific.Controllers
         }
 
         //Post: Create
-        //******************************** Need to make a lead view model *************************************
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID, Name, Phone, Email, Comment")] Lead lead)
-        {
+        public async Task<IActionResult> Create([Bind("ID, Name, Phone, Email, Comment, Date")] Lead lead)
+        {   
             if (ModelState.IsValid)
             {
+                lead.Date = DateTime.UtcNow;
+                
                 _Context.Add(lead);
                 await _Context.SaveChangesAsync();
                 return RedirectToAction(("/Home/Index"));
--- Pacific/Pacific/Models/Lead.cs
+++ Pacific/Pacific/Models/Lead.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,12 +10,17 @@ namespace Pacific.Models
     {
         public int ID { get; set; }
         
+        [Required]
         public string Name { get; set; }
 
-        public int Phone { get; set; }
+        [Required]
+        public string Phone { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
         public string Comment { get; set; }
+
+        public DateTime Date { get; set; } 
     }
 }
