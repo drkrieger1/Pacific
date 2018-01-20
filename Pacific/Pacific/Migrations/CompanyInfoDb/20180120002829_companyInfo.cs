98489c1cbfe51fa0603a431d8e64e267ab28be01
drkrieger1 <eplyushko@gmail.com>
2018-01-19 16:34:51 -0800

    Company info migration complete

---
+++ Pacific/Pacific/Migrations/CompanyInfoDb/20180120002829_companyInfo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Pacific.Migrations.CompanyInfoDb
+{
+    public partial class companyInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompanyInfo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AdressLine1 = table.Column<string>(nullable: true),
+                    AdressLine2 = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Mission = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<int>(nullable: false),
+                    State = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyInfo", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyInfo");
+        }
+    }
+}
